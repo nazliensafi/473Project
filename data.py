a7656ca8bdf5005b82346105ce4f78283a5758c7
Nazli Ensafi <n.ensafi87@gmail.com>
2022-11-16 15:21:10 -0500

    Add files via upload

--- data.py
+++ data.py
@@ -3,6 +3,7 @@
 import numpy as np  # linear algebra
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 import os
+from sklearn.model_selection import train_test_split
 
 
 data_filepath = os.path.join(os.getcwd(), "..", "data.csv")
