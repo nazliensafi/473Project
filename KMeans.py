05d59742ae2fd272084f042e280d079204821741
Victor T <45188433+MKB-1@users.noreply.github.com>
2022-11-18 14:37:15 -0500

    added docs to kmeans

--- KMeans.py
+++ KMeans.py
@@ -13,7 +13,15 @@ class KMeans:
         self.centroid_history = []
         pass
 
-    def _cluster_samples(self, data, centroids_df, distance_fx):
+    def _assign_cluster_to_samples(self, data, centroids_df, distance_fx):
+        """
+        Iterates over every sample and finds closest cluster based on distance function.
+        Returns DataFrame with 'cluster id' and 'centroid distance'.
+        :param data:
+        :param centroids_df:
+        :param distance_fx:
+        :return:
+        """
         self.centroid_history.append(centroids_df)
         clusters_df = pandas.DataFrame()
         for sample_index, sample in data.iterrows():
@@ -42,16 +50,30 @@ class KMeans:
         return sqrt_squared_sum
 
     def _next_centroids(self, current_clusters_df):
+        """
+        Calculates the average of every feature of every sample in the same cluster.
+        Returns new centroid for the cluster.
+        :param current_clusters_df:
+        :return:
+        """
         data = DATA.X_train.merge(current_clusters_df, left_index=True, right_index=True)
         next_centroids = data.groupby(['cluster id']).mean()
         return next_centroids
 
     def runner(self):
+        """
+        Initializes centroids to random samples. Assigns a cluster to every sample, based on distance to centroid.
+        Calculates new centroid for every cluster, based on samples belonging to that cluster.
+        Assigns a new cluster to every sample, based on the updated centroids.
+        If all samples remain in the same cluster, terminate.
+        Else, recalculate cluster centroids, and repeat.
+        :return:
+        """
         current_centroids_df = DATA.X_train.sample(self.k)
-        current_clusters_df = self._cluster_samples(DATA.X_train, current_centroids_df, self._distance)
+        current_clusters_df = self._assign_cluster_to_samples(DATA.X_train, current_centroids_df, self._distance)
         while True:
             next_centroids_df = self._next_centroids(current_clusters_df)
-            next_clusters_df = self._cluster_samples(DATA.X_train, current_centroids_df, self._distance)
+            next_clusters_df = self._assign_cluster_to_samples(DATA.X_train, current_centroids_df, self._distance)
 
             current_clusters_df = next_clusters_df
             current_centroids_df = next_centroids_df
